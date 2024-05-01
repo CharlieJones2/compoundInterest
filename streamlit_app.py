20e99ac6c7c6d2b979de1848dc19a96acf0c73a8
Charlie Jones <125832247+CharlieJones2@users.noreply.github.com>
2024-05-01 20:04:56 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,7 @@ if calculate:
     st.session_state.calculate = True
     st.session_state.calculatePressed = True
 
-    total_value = compound_interest(initial, rate, years, monthly_contribution)[0]
-    total_contribution = compound_interest(initial, rate, years, monthly_contribution)[1]
+    total_value, total_contribution, years_list, values_list, contributions = compound_interest(initial, rate, years, monthly_contribution)
     st.write(f'After {years} years, your saving will be worth £{round(total_value,2)}, having earned interest of £{round(total_value-total_contribution,2)}')
     df = pd.DataFrame({'Year': years_list, 'Value': values_list, 'Contribution': contributions})
 
